2be5ea7c5456c8bd1fb3e42d5091ad745955c276
klandestino7 <klandestino6@hotmail.com>
2021-07-30 19:46:09 -0300

    fix(creator): correção da ui que não fecha

--- resources/[FAROESTE]/[SYSTEM]/frp_creator/client/html/script.js
+++ resources/[FAROESTE]/[SYSTEM]/frp_creator/client/html/script.js
@@ -137,8 +137,10 @@ $(".button-left").on('click', function() {
 
 });
 function confirm(){
-    $.post('http://frp_creator/CloseCreator')
     $("#creatormenu").fadeOut(500);
+    $("#creatormenu").css("display","none");
+
+    $.post('http://frp_creator/CloseCreator');
 }
 
 function changeoptions(name) {
